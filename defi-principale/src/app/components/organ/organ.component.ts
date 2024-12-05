523065edc639168b5cf705bd1c4b028d9cae6a11
AYMAN <126827455+AymanMady@users.noreply.github.com>
2024-12-05 22:51:52 +0000

    get organ

---
+++ defi-principale/src/app/components/organ/organ.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organ',
+  imports: [],
+  templateUrl: './organ.component.html',
+  styleUrl: './organ.component.css'
+})
+export class OrganComponent {
+
+}
